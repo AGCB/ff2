7d558689796382f3a7b7fae805735a8305654df0
agcb <13634063+AGCB@users.noreply.github.com>
2025-03-20 20:25:45 -0700

    SCRUM-4-1 first linting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,55 +1,68 @@
-"use client"
-import { useEffect, useState } from 'react';
+"use client";
+import { useEffect, useState } from "react";
 
 const CONSTS = {
-  CONTINUE_BUTTON: 'Continue',
-  LICENSE_INFO: 'This app is open-source and available for use, modification, and distribution by anyone for any purpose.',
-  START_BUTTON: 'Start',
-  TITLE: 'FF2',
-}
+  CONTINUE_BUTTON: "Continue",
+  LICENSE_INFO:
+    "This app is open-source and available for use, modification, and distribution by anyone for any purpose.",
+  START_BUTTON: "Start",
+  TITLE: "FF2",
+};
 
 const Title = (
   <div>
     <h1 className="h-1/2 flex items-center">{CONSTS.TITLE}</h1>
-  </div>)
+  </div>
+);
 
-const StartButton: React.FC<{current: boolean}> = ({current}) => {
+const StartButton: React.FC<{ current: boolean }> = ({ current }) => {
   return (
     <>
-      <button className="min-w-30 text-left">{`${CONSTS.START_BUTTON.toUpperCase()} ${current ? '👈': ''}`}</button>
+      <button className="min-w-30 text-left">{`${CONSTS.START_BUTTON.toUpperCase()} ${
+        current ? "👈" : ""
+      }`}</button>
     </>
-  )
-}
+  );
+};
 
-const ContinueButton: React.FC<{current: boolean}> = ({current}) => {
+const TestComponent = () => {
   return (
     <>
-      <button className="min-w-30 text-left">{`${CONSTS.CONTINUE_BUTTON.toUpperCase()} ${current ? '👈': ''}`}</button>
+      <span>33333</span>
+      <span>1</span>
+      <span>3</span>
     </>
-  )
-}
-const LicenseInfo = (
-  <span>{CONSTS.LICENSE_INFO}</span>
-)
+  );
+};
 
+const ContinueButton: React.FC<{ current: boolean }> = ({ current }) => {
+  return (
+    <>
+      <button className="min-w-30 text-left">{`${CONSTS.CONTINUE_BUTTON.toUpperCase()} ${
+        current ? "👈" : ""
+      }`}</button>
+    </>
+  );
+};
+const LicenseInfo = <span>{CONSTS.LICENSE_INFO}</span>;
 
 export default function Home() {
-  const [selection, setSelection] = useState('start');
+  const [selection, setSelection] = useState("start");
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
-      if(event.key === 'ArrowLeft') {
-        setSelection('start')
-      } else if (event.key === 'ArrowRight') {
-        setSelection('continue')
+      if (event.key === "ArrowLeft") {
+        setSelection("start");
+      } else if (event.key === "ArrowRight") {
+        setSelection("continue");
       }
-    }
+    };
 
-    window.addEventListener('keydown', handleKeyDown)
+    window.addEventListener("keydown", handleKeyDown);
 
     return () => {
-      window.removeEventListener('keydown', handleKeyDown)
-    }
-  },[]);
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
   return (
     <div className="border-4 border-red-500 w-screen h-screen flex flex-col items-center gap-4">
       <div className="bg-gradient-to-r from-purple-600 to-blue-400 h-75 w-full ">
@@ -57,8 +70,8 @@ export default function Home() {
       </div>
       <div className="border-4 border-blue-400">
         <div className="border-4 border-yellow-400 flex justify-around">
-          {<StartButton current={selection === 'start'}/>}
-          {<ContinueButton current={selection === 'continue'} />}
+          {<StartButton current={selection === "start"} />}
+          {<ContinueButton current={selection === "continue"} />}
         </div>
         {LicenseInfo}
       </div>
